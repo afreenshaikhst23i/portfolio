0ebf2d8a5bd2d3e0d48b2b7dad6d0c8128ed5a92
afreenshaikhst23i <46025523+afreenshaikhst23i@users.noreply.github.com>
2025-08-08 03:07:43 +0530

    working on projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,62 +1,54 @@
+import goodluckScreen from '../assets/images/projects/goodluck.png'
+import portfolioScreen from '../assets/images/projects/portfolio.png'
+import githubWhiteIcon from '../assets/images/github-white.svg'
+import eLinkWhiteIcon from '../assets/images/external-link-white.svg'
+
 function Projects() {
     return (
-        <section class="Projects-section" id="projects">
-            <div class="heading-wrapper">
-                <h2 class="primary-heading">Projects</h2>
-                <span class="primary-heading-shadow" aria-hidden="true">Projects</span>
-                <p class="primary-subheading">Here are a few past design projects I've worked on.</p>
+        <section className="Projects-section" id="projects">
+            <div className="heading-wrapper">
+                <h2 className="primary-heading">Projects</h2>
+                <span className="primary-heading-shadow" aria-hidden="true">Projects</span>
             </div>
-            <div class="projects-wrap">
-                {/* <ul class="projects-slider">
+            <div className="projects-wrap">
+                <ul className="projects-slider">
                     <li>
                         <figure>
+                            <img className="project-thumb" src={goodluckScreen} alt="" />
                             <figcaption>
-                                <h2 class="fig-name">Mr. Electric</h2>
-                                <p class="fig-text">A global franchise organization providing electrical installation and repair services.</p>
-                                <a class="fig-link" href="https://mrelectric.com/" target="_blank">
-                                    Visit Website
-                                </a>
+                                <h2 className="fig-name">Good Luck Digital Photo Studio</h2>
+                                <p className="fig-text">Designed and developed a portfolio website for a photo studio to professionally showcase their photography and services.</p>
+                                <p className="mt-4 text-secondary">html5 , tailwind CSS</p>
+                                <div className='flex flex-row gap-4 items-center'>
+                                    <a className="fig-link" href="https://github.com/afreenshaikhst23i/goodluck-2024" target="_blank">
+                                        <img className="w-6 h-6" src={githubWhiteIcon} alt="" />
+                                    </a>
+                                    <a className="fig-link" href="https://goodluckdps.com/" target="_blank">
+                                        <img className="w-6 h-6" src={eLinkWhiteIcon} alt="" />
+                                    </a>
+                                </div>
                             </figcaption>
-                            <img class="project-thumb" src="./images/projects/mr-electric.png" alt="">
                         </figure>
                     </li>
                     <li>
                         <figure>
+                            <img className="project-thumb" src={portfolioScreen} alt="" />
                             <figcaption>
-                                <h2 class="fig-name">Good Luck Digital Photo Studio</h2>
-                                <p class="fig-text">A global franchise organization providing electrical installation and repair services.</p>
-                                <a class="fig-link" href="https://goodluckdps.com/" target="_blank">
-                                    Visit Website
-                                </a>
+                                <h2 className="fig-name">My Portfolio Website</h2>
+                                <p className="fig-text">Loosely designed in VS Code and fully hand-coded using Tailwind CSS. Deployed via GitHub Pages.</p>
+                                <p className="mt-4 text-secondary">React.js , vite , tailwind CSS , github pages</p>
+                                <div className='flex flex-row gap-4 items-center'>
+                                    <a className="fig-link" href="https://github.com/afreenshaikhst23i/portfolio" target="_blank">
+                                        <img className="w-6 h-6" src={githubWhiteIcon} alt="" />
+                                    </a>
+                                    <a className="fig-link" href="https://afreenshaikhst23i.github.io/portfolio/" target="_blank">
+                                        <img className="w-6 h-6" src={eLinkWhiteIcon} alt="" />
+                                    </a>
+                                </div>
                             </figcaption>
-                            <img class="project-thumb" src="./images/projects/good-luck.png" alt="">
                         </figure>
                     </li>
-                    <li>
-                        <figure>
-                            <figcaption>
-                                <h2 class="fig-name">Window Genie</h2>
-                                <p class="fig-text">A global franchise organization providing electrical installation and repair services.</p>
-                                <a class="fig-link" href="https://www.windowgenie.com/" target="_blank">
-                                    Visit Website
-                                </a>
-                            </figcaption>
-                            <img class="project-thumb" src="./images/projects/window-genie.png" alt="">
-                        </figure>
-                    </li>
-                    <li>
-                        <figure>
-                            <figcaption>
-                                <h2 class="fig-name">Rainbow Restoration</h2>
-                                <p class="fig-text">A global franchise organization providing electrical installation and repair services.</p>
-                                <a class="fig-link" href="https://www.windowgenie.com/" target="_blank">
-                                    Visit Website
-                                </a>
-                            </figcaption>
-                            <img class="project-thumb" src="./images/projects/rainbow-restoration.png" alt="">
-                        </figure>
-                    </li>
-                </ul> */}
+                </ul>
             </div>
         </section>
     );
